a28601646d100d2135e6df60eafb05bf9b242f9a
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-17 20:40:11 -0300

    appOrPessoal-part3

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -45,6 +45,33 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+    recuperarTodosRegistros(){
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar todas as despesas cadastradas em localStorage
+        for(let i = 1; i <= id; i++){
+
+            //recuperar a despesa transformando de JSON para obj literal
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //se existe a possibilidade de haver índices pulados
+            if(despesa === null){
+                continue
+            }
+
+
+            despesas.push(despesa)
+        }
+        return despesas
+    }
+
+    pesquisar(despesa){
+
+    }
 }
 
 let bd = new Bd()
@@ -82,7 +109,16 @@ function cadastrarDespesa(){
         
 
         //dialog de sucesso
-        $('#modalRegistraDespesa').modal('show')   
+        $('#modalRegistraDespesa').modal('show') 
+        
+        //zerando as caixas após receber valor
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+
     } else{
         //dialog de erro
 
@@ -97,4 +133,66 @@ function cadastrarDespesa(){
 
 }
 
+//função que vai carregar a lista de despesas para ficar visível ao usuário
+function carregaListaDespesas(){
+
+    //recuperar os valores e colocá-los em uma variável
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+    
+    //selecionando o elemento tbody da tabela
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    /* 
+    <tr>
+        <td>15/03/2018</td>
+        <td>alimentação</td>
+        <td>compras do mês</td>
+        <td>556</td>
+    </tr> 
+    */
+
+    //percorrer o array despesas,listando cada despesa de forma dinâmica
+    despesas.forEach(function(d){
+        
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${ d.ano}`
+        //ajustar o tipo
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação'
+                break
+            case '2': d.tipo = 'Educação'
+                break
+            case '3': d.tipo = 'Lazer'
+                break
+            case '4': d.tipo = 'saúde'
+                break
+            case '5': d.tipo = 'Transporte'
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = `R$${d.valor}`
+    })
+}
+
+//filtrar despesas
+function pesquisarDespesa(){
+
+    //resgatando os valores e atribuindo eles a uma variável
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano,mes,dia,tipo,descricao,valor)
+
+    bd.pesquisar(despesa)
+}
 

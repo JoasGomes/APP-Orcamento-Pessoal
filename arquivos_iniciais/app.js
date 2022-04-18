320f3b3f1c32af9d3472267ab2f40adf053f87b2
JoasGomes <jojoca.vitor777@gmail.com>
2022-04-18 20:11:15 -0300

    appOrPessoal-part4

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -71,6 +71,38 @@ class Bd {
 
     pesquisar(despesa){
 
+        //criando um array com as despesas de atribuindo a uma variável
+        let despesasFiltradas = Array()
+
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+
+        //filtros
+        //ano
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+        //mes
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+        //dia
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }        
+        //tipo
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }        
+        //descricao
+        if(despesa.descricao != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }        
+        //valor
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }  
+        console.log(despesasFiltradas)  
     }
 }
 
